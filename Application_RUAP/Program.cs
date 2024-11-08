f59e405c6d702905b1d870ab10de782eb2c8294d
helenaborzan <helena.borzan@gmail.com>
2024-11-08 15:36:36 +0100

    change in app

---
+++ Application_RUAP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Application_RUAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("„Pozdrav iz aplikacije od Helena Borzan, broj indeksa 4847848");
+        }
+    }
+}
